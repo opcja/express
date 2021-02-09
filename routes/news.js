5aa8fd009d26d82982b24be1ced8af952ccc9b48
opcja <piotrmajda7@gmail.com>
2021-02-09 17:04:08 +0100

    news filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('news', { title: 'News' });
+  const search = req.query.search || '';
+
+  const findNews = News
+    .find({title: new RegExp(search.trim(), 'i')}) // nie bierze pod uwage wielkosci liter i szuka "po części" trim usuwa spacje przód
+    .sort({created: -1}) // rosnąco by było 1
+
+  findNews.exec((err, data) => {
+    res.render('news', { title: 'News', data, search });
+  });  
 });
 
 module.exports = router;
